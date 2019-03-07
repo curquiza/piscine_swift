cdf6c9e10e359306f8cfb8785d9f336046dc49b3
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-03-07 17:42:28 +0100

    ex01

---
+++ d01/ex01/Card.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+class Card : NSObject {
+
+    var color: Color
+    var value: Value
+
+    init (color: Color, value: Value) {
+        print("Card constructor called.")
+        self.color = color
+        self.value = value
+    }
+
+    override var description: String {
+        return "Card description -> Color : " + color.rawValue + ", Value : " + String(value.hashValue)
+    }
+
+    override func isEqual(_ object: Any?) -> Bool {
+        if let card = object as? Card {
+            if (card.color == self.color && card.value == self.value) {
+                return true
+            }
+        }
+        return false
+    }
+
+    static func ==(lhs: Card, rhs: Card) -> Bool {
+        return lhs.isEqual(rhs)
+    }
+}

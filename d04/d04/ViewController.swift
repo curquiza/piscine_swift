32740495fde35c02e2b7344c578f0abe1e6dfa72
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-03-15 18:37:02 +0100

    d04 in progress le retour

--- d04/d04/ViewController.swift
+++ d04/d04/ViewController.swift
@@ -48,5 +48,41 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.tweet = TweetData.tweets[indexPath.row]
         return cell
     }
+    
+    @IBAction func titiButton(_ sender: UIButton) {
+        getToken()
+    }
+    func getToken() {
+        if let api_key = ProcessInfo.processInfo.environment["TWITTER_API_KEY"] {
+            if let api_secret_key = ProcessInfo.processInfo.environment["TWITTER_API_SECRET_KEY"] {
+                let bearer = ((api_key + ":" + api_secret_key).data(using: String.Encoding.utf8))!.base64EncodedString(options: NSData.Base64EncodingOptions (rawValue: 0))
+                
+                let url = NSURL(string: "https://api.twitter.com/oauth2/token")
+                let request = NSMutableURLRequest(url: url! as URL)
+                request.httpMethod = "POST"
+                request.setValue("Basic " + bearer, forHTTPHeaderField: "Authorization")
+                request.setValue("application/x-www-form-urlencoded;charset=UTF-8", forHTTPHeaderField: "Content-Type")
+                request.httpBody = "grant_type=client_credentials".data(using: String.Encoding.utf8)
+                let task = URLSession.shared.dataTask(with: request as URLRequest) {
+                    (data, response, error) in
+//                    print(response!)
+                    if let err = error {
+                        print(err)
+                    }
+                    else if let d = data {
+                        do {
+                            if let dic: NSDictionary = try JSONSerialization.jsonObject(with: d, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
+                            print(dic)
+                            }
+                        }
+                        catch (let err){
+                            print(err)
+                        }
+                    }
+                }
+                task.resume()
+            }
+        }
+    }
 }
 

6ab31673f6cbc05c78c891deff42a7f03211b3cd
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-03-22 15:54:30 +0100

    first commit d05, basic project

---
+++ d05/d05/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  d05
+//
+//  Created by Clementine URQUIZAR on 3/22/19.
+//  Copyright © 2019 Clementine URQUIZAR. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

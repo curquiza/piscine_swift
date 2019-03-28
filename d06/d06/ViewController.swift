ecfa7e3889d58dcf88ba4d97b8bb6712aa629536
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-03-28 16:52:22 +0100

    add d06 ex01

--- d06/d06/ViewController.swift
+++ d06/d06/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var dynamicAnimator: UIDynamicAnimator!
+    var gravityBehavior: UIGravityBehavior!
+    var collisionBehavior: UICollisionBehavior!
+    var itemBehavior: UIDynamicItemBehavior!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,31 +22,62 @@ class ViewController: UIViewController {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
         view.addGestureRecognizer(tapGesture)
         
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Init pan gesture
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        view.addGestureRecognizer(panGesture)
+        
+        dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
+        
+        // Gravity behaviour
+        gravityBehavior = UIGravityBehavior()
+        dynamicAnimator.addBehavior(gravityBehavior)
+        
+        // Collision behaviour
+        collisionBehavior = UICollisionBehavior()
+        collisionBehavior.setTranslatesReferenceBoundsIntoBoundary(with: self.view.alignmentRectInsets)
+        dynamicAnimator.addBehavior(collisionBehavior)
+        
+        // Item Behavior
+        itemBehavior = UIDynamicItemBehavior()
+        itemBehavior.elasticity = 0.6
+        dynamicAnimator.addBehavior(itemBehavior)
     }
     
     @objc func handleTap(sender: UITapGestureRecognizer) {
         if sender.state == .ended {
             print("Tapped")
             let touchedPoint = sender.location(in: self.view)
-            addNewShape(posX: touchedPoint.x, posY: touchedPoint.y)
+            addNewShape(x: touchedPoint.x, y: touchedPoint.y)
         }
     }
     
-    func addNewShape(posX: CGFloat, posY: CGFloat) {
+    @objc func handlePan(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .began:
+            print("began")
+        case .changed:
+            print("changed to \(gesture.location(in: self.view))")
+        case .ended:
+            print("ended")
+        case .failed, .cancelled:
+            print("failed/cancelled")
+        case .possible:
+            print("possible")
+        }
+    }
+    
+    func addNewShape(x: CGFloat, y: CGFloat) {
         print("Add new shape")
-        let newShape = UIView(frame: CGRect(x: posX - 50, y: posY - 50, width: 100, height: 100))
-        
+        let newShape = UIView(frame: CGRect(x: x - 50, y: y - 50, width: 100, height: 100))
         if (arc4random_uniform(2) == 1) {
           newShape.layer.cornerRadius = 50
         }
         newShape.backgroundColor = getShapeUIColor()
-        
         self.view.addSubview(newShape)
+        
+        itemBehavior.addItem(newShape)
+        gravityBehavior.addItem(newShape)
+        collisionBehavior.addItem(newShape)
     }
     
     func getShapeUIColor() -> UIColor {
@@ -63,4 +99,3 @@ class ViewController: UIViewController {
     }
 
 }
-
